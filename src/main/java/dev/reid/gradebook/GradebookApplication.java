ad488682711028975d95dbecfae53535d796014c
Kurtis <kreid8@kent.edu>
2022-08-31 17:47:26 -0400

    added tests

--- src/main/java/dev/reid/gradebook/GradebookApplication.java
+++ src/main/java/dev/reid/gradebook/GradebookApplication.java
@@ -2,8 +2,14 @@ package dev.reid.gradebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.reid"})
+@EntityScan(basePackages = {"dev.reid.entities"})
+@EnableJpaRepositories(basePackages = {"dev.reid.repos"})
 public class GradebookApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/dev/reid/gradebook/StudentRepoTests.java
@@ -0,0 +1,25 @@
+package dev.reid.gradebook;
+
+import dev.reid.entities.Student;
+import dev.reid.repos.StudentRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentRepoTests {
+    /*
+    @Autowired
+    StudentRepo studentRepo;
+    @Test
+    public void create_student()
+    {
+        Student student = new Student(1,"kh", "rg", "GU");
+        Student savedStudent = this.studentRepo.save(student);
+        Assertions.assertEquals(1, savedStudent.getId());
+        System.out.println(savedStudent);
+    }
+    */
+
+}
