b27bf52557bcaf303d26cede1f5735f38d40c4f0
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-09-01 11:11:31 -0700

    Implemented student mockitos test for add students

--- src/test/java/dev/reid/mockitoTests/StudentMockitos.java
+++ src/test/java/dev/reid/mockitoTests/StudentMockitos.java
@@ -1,26 +1,45 @@
 package dev.reid.mockitoTests;
 
 
+import dev.reid.entities.Student;
+import dev.reid.services.StudentServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class StudentMockitos {
 
-    @Test
-    void student_must_have_all_fields_filled(){
-
-    }
+    @Autowired
+    StudentServiceImpl studentServiceImpl;
 
     @Test
-    void get_student_by_id(){
+    void add_student_to_list(){
+    StudentServiceImpl studentService = Mockito.mock(StudentServiceImpl.class);
+    Student student = new Student(0, "Billy", "Idol", "Elizabeth Idol");
+
+    Mockito.when(studentService.addStudent(student)).thenReturn(student);
+    Assertions.assertThrows(RuntimeException.class, ()->{
+        studentService.addStudent(student);
+    });
     }
 
-    @Test
-    void get_student_by_name(){
-    }
-
-    @Test
-    void delete_student_by_id(){
-    }
+//    @Test
+//    void get_all_students(){
+//
+//    }
+//
+//    @Test
+//    void get_student_by_id(){
+//    }
+//
+//    @Test
+//    void get_student_by_name(){
+//    }
+//
+//    @Test
+//    void delete_student_by_id(){
+//    }
 
 
 }
