9d260789e805771062bbdfdb79e11166afbaafa2
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-09-01 09:31:05 -0700

    Initialized mockito tests

---
+++ src/test/java/dev/reid/mockitoTests/StudentMockitos.java
@@ -0,0 +1,26 @@
+package dev.reid.mockitoTests;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StudentMockitos {
+
+    @Test
+    void student_must_have_all_fields_filled(){
+
+    }
+
+    @Test
+    void get_student_by_id(){
+    }
+
+    @Test
+    void get_student_by_name(){
+    }
+
+    @Test
+    void delete_student_by_id(){
+    }
+
+
+}
