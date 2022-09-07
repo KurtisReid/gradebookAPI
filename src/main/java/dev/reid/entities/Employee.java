e2d54f26116a1c7a84dbf64a4c8d01cfcaeba28c
Kurtis <kreid8@kent.edu>
2022-09-07 15:35:29 -0400

    Added dependencies and Employee entity

---
+++ src/main/java/dev/reid/entities/Employee.java
@@ -0,0 +1,66 @@
+package dev.reid.entities;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("teachers")
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String role;
+
+    public Employee() {
+    }
+
+    public Employee(String id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
