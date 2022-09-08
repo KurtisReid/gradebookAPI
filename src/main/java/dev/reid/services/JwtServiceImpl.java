e566c8d7fde8eaf9466448b46d1c52ff2b2983b0
Kurtis <kreid8@kent.edu>
2022-09-08 15:27:20 -0400

    publish a message to the Important Event Queue with the time of login and the user

--- src/main/java/dev/reid/services/JwtServiceImpl.java
+++ src/main/java/dev/reid/services/JwtServiceImpl.java
@@ -3,16 +3,26 @@ package dev.reid.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class JwtServiceImpl implements JwtService{
 
     private Algorithm algorithm = Algorithm.HMAC256("Live long and prosper");
 
+    @Autowired
+    JmsTemplate jmsTemplate;
+
 
     @Override
     public String createJwtWithUsernameAndRole(String username, String role) {
+        String msg = username + " Login time:" + LocalDateTime.now();
+        jmsTemplate.convertAndSend("message-queue", msg);
+        System.out.println(msg);
         String jwt = JWT.create().withClaim("username", username).withClaim("role", role).sign(algorithm);
         return jwt;
     }
