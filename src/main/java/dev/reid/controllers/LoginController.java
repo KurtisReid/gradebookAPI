594f636b835b9923f432f278c86b6b553ce588e9
Kurtis <kreid8@kent.edu>
2022-09-09 12:36:07 -0400

    Fixed error with delete and login controller

--- src/main/java/dev/reid/controllers/LoginController.java
+++ src/main/java/dev/reid/controllers/LoginController.java
@@ -3,19 +3,26 @@ package dev.reid.controllers;
 import dev.reid.dtos.LoginCredentials;
 import dev.reid.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class LoginController {
     @Autowired
     LoginService loginService;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginCredentials loginCredentials)
+    public ResponseEntity<String> login(@RequestBody LoginCredentials loginCredentials)
     {
+        System.out.println("login controller");
+        String jw = loginService.authenticateUser(loginCredentials);
+        System.out.println(loginService.authenticateUser(loginCredentials));
 
-        return loginService.authenticateUser(loginCredentials);
+        return new ResponseEntity<>(jw, HttpStatus.CONTINUE);
     }
 }
